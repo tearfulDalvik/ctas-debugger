3409319c419d15ef4b620ab3a22de1c1a64e6008
Dalvik Shen <meizaizheli@ifengge.cn>
2018-12-08 23:33:36 +0800

    update(offline): solved problems

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 @echo off
-
+SET bat_path=%~dp0
 echo Copyright Dalvik Shen 2018
 echo.
 
@@ -8,10 +8,21 @@ set "PYTHON_VERSION=3.7.1"
 python -V && echo Installation Detected && echo. || goto py
 goto pip
 
+
 :py
   echo Installing Python
-  python-%PYTHON_VERSION%.exe /quiet
+  echo REMEMBER TO CHECK THE BOX 'ADD TO PATH'
+  %bat_path:~0,-1%\python-%PYTHON_VERSION%.exe
+  echo Rerun this script to continue
+  pause
+  exit /B 1
 
 :pip
-pip download -d whl -r requirements.txt
+echo Installing dependents
+pip install %bat_path:~0,-1%\whl\pyperclip-1.7.0.tar.gz
+echo.
+echo Done.
+echo Just double click __main__.pyw to run debugger
+echo.
+pause
 exit /B 0
